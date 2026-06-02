a46203049d1b0858ee9bf3cfed1543d11b9210eb
sagorsanto49 <santosagor49@gmail.com>
2026-06-02 22:25:21 +0600

    Changes

--- lib/feature/signin/presentation/page/unpaid.dart
+++ lib/feature/signin/presentation/page/unpaid.dart
@@ -1,15 +1,15 @@
-import 'package:doctor/feature/signin/presentation/page/feadback.dart';
 import 'package:flutter/material.dart';
 import 'package:doctor/core/widget/primarybutten.dart';
-import 'package:doctor/core/widget/xfield.dart';
 import 'package:doctor/core/widget/text.dart';
+import 'package:doctor/core/widget/xfield.dart';
+import 'package:doctor/feature/signin/presentation/page/feadback.dart';
 
 class UnPaidScreen extends StatelessWidget {
   const UnPaidScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    final List<Map<String, dynamic>> clients = [
+    final clients = [
       {"name": "Client 01", "amount": 150},
       {"name": "Client 02", "amount": 150},
       {"name": "Client 03", "amount": 150},
@@ -23,160 +23,142 @@ class UnPaidScreen extends StatelessWidget {
       appBar: AppBar(
         backgroundColor: Colors.white,
         centerTitle: true,
-        title: const Text("UnPaid"),
+        elevation: 1,
+        title: const Text(
+          "UnPaid",
+          style: TextStyle(color: Colors.black),
+        ),
         leading: IconButton(
           onPressed: () => Navigator.pop(context),
-          icon: const Icon(Icons.arrow_back),
+          icon: const Icon(Icons.arrow_back, color: Colors.black),
         ),
       ),
 
-      body: Padding(
-        padding: const EdgeInsets.all(12),
-        child: Column(
-          children: [
-            /// Client List
-            Expanded(
-              child: ListView(
-                children: [
-                  Container(
-                    decoration: BoxDecoration(
-                      border: Border.all(
-                        color: Colors.green.shade100,
-                      ),
-                    ),
-                    child: Column(
-                      children: clients.map((client) {
-                        return Container(
-                          padding: const EdgeInsets.all(10),
-                          decoration: BoxDecoration(
-                            border: Border(
-                              bottom: BorderSide(
-                                color: Colors.green.shade100,
-                              ),
-                            ),
-                          ),
-                          child: Row(
-                            crossAxisAlignment:
-                                CrossAxisAlignment.start,
-                            children: [
-                              Expanded(
-                                child: Column(
-                                  crossAxisAlignment:
-                                      CrossAxisAlignment.start,
-                                  children: [
-                                    Texts(
-                                      text: client["name"],
-                                    ),
-
-                                    const SizedBox(height: 6),
-
-                                    Texts(
-                                      text: "14 jan 2025",
-                                      // fontSize: 12,
-                                      color: Colors.grey,
-                                    ),
-                                  ],
-                                ),
-                              ),
-
-                              Texts(
-                                text:
-                                    "${client["amount"]} \$",
-                                color: Colors.red,
-                                // fontSize: 18,
-                              ),
-                            ],
-                          ),
-                        );
-                      }).toList(),
-                    ),
-                  ),
-
-                  const SizedBox(height: 20),
-
-                  /// Total
-                  Padding(
-                    padding: const EdgeInsets.symmetric(
-                      horizontal: 10,
-                    ),
-                    child: Row(
-                      children: [
-                        Expanded(
-                          child: Column(
-                            crossAxisAlignment:
-                                CrossAxisAlignment.start,
-                            children: const [
-                              Texts(
-                                text: "Total",
-                              ),
-
-                              SizedBox(height: 6),
-
-                              Texts(
-                                text: "14 jan 2025",
-                                // fontSize: 12,
-                                color: Colors.grey,
-                              ),
-                            ],
-                          ),
-                        ),
-
-                        const Texts(
-                          text: "750 \$",
-                          color: Colors.red,
-                          // fontSize: 22,
-                        ),
-                      ],
-                    ),
-                  ),
-
-                  const SizedBox(height: 50),
-
-                  /// Email Section
-                  Container(
-                    padding: const EdgeInsets.all(12),
-                    decoration: BoxDecoration(
-                      color: const Color(0xffB8D7F0),
-                      borderRadius:
-                          BorderRadius.circular(4),
-                    ),
-                    child: Column(
-                      children: [
-                        const Xfield(
-                          text:
-                              "someone@gmail.com",
-                        ),
-
-                        const SizedBox(height: 18),
-
-                        const Align(
-                          alignment:
-                              Alignment.centerLeft,
-                          child: Texts(
-                            text:
-                                "Data will be sent to the email above.",
-                            // fontSize: 13,
-                          ),
-                        ),
-
-                        const SizedBox(height: 20),
-
-                        SizedBox(
-                          width: 140,
-                          child: Primarybutten(
-                            title: "Send", onpress: () {
-                              Navigator.push(context, MaterialPageRoute(builder: (context)=>FeedbackPage()));
-                              },
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                ],
+      body: ListView(
+        padding: const EdgeInsets.all(16),
+        children: [
+          /// Client List
+          Container(
+            decoration: BoxDecoration(
+              color: Colors.white,
+              border: Border.all(
+                color: Colors.green.shade100,
               ),
+              borderRadius: BorderRadius.circular(8),
             ),
-          ],
-        ),
+            child: ListView.separated(
+              shrinkWrap: true,
+              physics: const NeverScrollableScrollPhysics(),
+              itemCount: clients.length,
+              separatorBuilder: (context, index) => Divider(
+                height: 1,
+                color: Colors.green.shade100,
+              ),
+              itemBuilder: (context, index) {
+                final client = clients[index];
+
+                return ListTile(
+                  title: Texts(
+                    text: client["name"].toString(),
+                  ),
+                  subtitle: const Texts(
+                    text: "14 Jan 2025",
+                    color: Colors.grey,
+                  ),
+                  trailing: Texts(
+                    text: "${client["amount"]} \$",
+                    color: Colors.red,
+                  ),
+                );
+              },
+            ),
+          ),
+
+          const SizedBox(height: 20),
+
+          /// Total Section
+          Container(
+            padding: const EdgeInsets.symmetric(
+              horizontal: 10,
+              vertical: 8,
+            ),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(8),
+            ),
+            child: Row(
+              children: [
+                const Expanded(
+                  child: Column(
+                    crossAxisAlignment:
+                        CrossAxisAlignment.start,
+                    children: [
+                      Texts(
+                        text: "Total",
+                      ),
+                      SizedBox(height: 5),
+                      Texts(
+                        text: "14 Jan 2025",
+                        color: Colors.grey,
+                      ),
+                    ],
+                  ),
+                ),
+
+                const Texts(
+                  text: "750 \$",
+                  color: Colors.red,
+                ),
+              ],
+            ),
+          ),
+
+          const SizedBox(height: 30),
+
+          /// Email Section
+          Container(
+            padding: const EdgeInsets.all(16),
+            decoration: BoxDecoration(
+              color: const Color(0xffB8D7F0),
+              borderRadius: BorderRadius.circular(8),
+            ),
+            child: Column(
+              children: [
+                const Xfield(
+                  text: "someone@gmail.com",
+                ),
+
+                const SizedBox(height: 15),
+
+                const Align(
+                  alignment: Alignment.centerLeft,
+                  child: Texts(
+                    text:
+                        "Data will be sent to the email above.",
+                  ),
+                ),
+
+                const SizedBox(height: 20),
+
+                SizedBox(
+                  width: 150,
+                  child: Primarybutten(
+                    title: "Send",
+                    onpress: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (_) =>
+                              const FeedbackPage(),
+                        ),
+                      );
+                    },
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
