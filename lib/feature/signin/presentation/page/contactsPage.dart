524181a0935d86ab64883cf9c8184e683944c457
sagorsanto49 <santosagor49@gmail.com>
2026-06-06 21:35:35 +0600

    Contacts Page

--- lib/feature/signin/presentation/page/contactsPage.dart
+++ lib/feature/signin/presentation/page/contactsPage.dart
@@ -5,6 +5,37 @@ class ContactsPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    final contacts = [
+      "Clients",
+      "Horse",
+      "Dog",
+    ];
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Contacts"),
+        centerTitle: true,
+      ),
+      body: ListView.separated(
+        itemCount: contacts.length,
+        separatorBuilder: (_, __) => const Divider(height: 1),
+        itemBuilder: (context, index) {
+          return ListTile(
+            leading: const Icon(Icons.account_circle_outlined),
+            title: Text(contacts[index]),
+            onTap: () {
+              if (contacts[index] == "Clients") {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (_) =>ContactsPage() ,
+                  ),
+                );
+              }
+            },
+          );
+        },
+      ),
+    );
   }
 }
