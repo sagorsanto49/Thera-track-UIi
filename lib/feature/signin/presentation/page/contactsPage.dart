e37254d7422ba3a6d539c940d3fa077c5bcafdcd
sagorsanto49 <santosagor49@gmail.com>
2026-06-07 15:43:26 +0600

    Horse

--- lib/feature/signin/presentation/page/contactsPage.dart
+++ lib/feature/signin/presentation/page/contactsPage.dart
@@ -1,12 +1,14 @@
-import 'package:doctor/feature/signin/presentation/page/clientspage.dart';
+import 'package:doctor/feature/signin/presentation/page/horse.dart';
 import 'package:flutter/material.dart';
+import 'package:doctor/feature/signin/presentation/page/clientspage.dart';
+// import 'package:doctor/feature/signin/presentation/page/horsepage.dart';
 
 class ContactsPage extends StatelessWidget {
   const ContactsPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    List<String> contacts = ["Clients", "Horse", "Dog"];
+    final List<String> contacts = ["Clients", "Horse", "Dog"];
 
     return Scaffold(
       appBar: AppBar(
@@ -24,6 +26,14 @@ class ContactsPage extends StatelessWidget {
                   context,
                   MaterialPageRoute(
                     builder: (context) => const ClientsPage(),
+                  ),
+                );
+              }
+               else if (contacts[index] == "Horse") {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => const Horse(),
                   ),
                 );
               }
