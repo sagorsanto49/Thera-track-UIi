59ab3f5639df2c093e8afb9cc09a8011715c4d42
sagorsanto49 <santosagor49@gmail.com>
2026-06-11 12:04:51 +0600

    Home 5.1

--- lib/feature/signin/presentation/page/home5.dart
+++ lib/feature/signin/presentation/page/home5.dart
@@ -1,6 +1,7 @@
 import 'package:doctor/core/widget/primarybutten.dart';
 import 'package:doctor/core/widget/xfield.dart';
 import 'package:doctor/feature/signin/presentation/page/equipment.dart';
+import 'package:doctor/feature/signin/presentation/page/home51.dart';
 import 'package:flutter/material.dart';
 
 class Home5 extends StatelessWidget {
@@ -11,10 +12,7 @@ class Home5 extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         leading: Icon(Icons.arrow_back, color: Colors.black),
-        title: Text(
-          'Step 5',
-          style: TextStyle(color: Colors.black),
-        ),
+        title: Text('Step 5', style: TextStyle(color: Colors.black)),
         centerTitle: true,
         backgroundColor: Colors.white,
         elevation: 1,
@@ -26,36 +24,55 @@ class Home5 extends StatelessWidget {
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                Text("Treatment's",style: TextStyle(fontSize: 20),),
-                Icon(Icons.eight_k_plus)
+                Text("Treatment's", style: TextStyle(fontSize: 20)),
+                Icon(Icons.eight_k_plus),
               ],
             ),
-            SizedBox(height: 10,),
-            Xfield(text: 'Cryotherapy',suffixIcons: IconButton(
-              onPressed: () {},
-              icon: Icon(Icons.align_horizontal_right),
-            ),),
-            SizedBox(height: 10,),  Xfield(text: 'Hydrotherapy',suffixIcons: IconButton(
-              onPressed: () {},
-              icon: Icon(Icons.align_horizontal_right),
-            ),),
-            SizedBox(height: 10,),  Xfield(text: 'Laser Therapy',suffixIcons: IconButton(
-              onPressed: () {},
-              icon: Icon(Icons.align_horizontal_right),
-            ),),
-            SizedBox(height: 10,),  Xfield(text: 'Osteopathy',suffixIcons: IconButton(
-              onPressed: () {},
-              icon: Icon(Icons.align_horizontal_right),
-            ),),
-            SizedBox(height: 30,),
-            Primarybutten(title: 'Next', onpress: (){
-              Navigator.push(context, MaterialPageRoute(builder: (context)=>Equipment() ));
-            })
-
+            SizedBox(height: 10),
+            Xfield(
+              text: 'Cryotherapy',
+              suffixIcons: IconButton(
+                onPressed: () {},
+                icon: Icon(Icons.align_horizontal_right),
+              ),
+            ),
+            SizedBox(height: 10),
+            Xfield(
+              text: 'Hydrotherapy',
+              suffixIcons: IconButton(
+                onPressed: () {},
+                icon: Icon(Icons.align_horizontal_right),
+              ),
+            ),
+            SizedBox(height: 10),
+            Xfield(
+              text: 'Laser Therapy',
+              suffixIcons: IconButton(
+                onPressed: () {},
+                icon: Icon(Icons.align_horizontal_right),
+              ),
+            ),
+            SizedBox(height: 10),
+            Xfield(
+              text: 'Osteopathy',
+              suffixIcons: IconButton(
+                onPressed: () {},
+                icon: Icon(Icons.align_horizontal_right),
+              ),
+            ),
+            SizedBox(height: 30),
+            Primarybutten(
+              title: 'Next',
+              onpress: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => Home51()),
+                );
+              },
+            ),
           ],
         ),
       ),
-
     );
   }
 }
